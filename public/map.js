7a6d2f6b049a202e870c4a8d9c9af6eb31b49c03
Jake Cupani <jakecup1@gmail.com>
2019-11-13 23:14:36 -0500

    basemap working

--- public/map.js
+++ public/map.js
@@ -1,7 +1,5 @@
-var map = L.map('mapid').setView([39.09, -76.83], 13);
-L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-    {
-        attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-        maxZoom: 17,
-        minZoom: 9
-    }).addTo(map);
+var mymap = L.map('mapid').setView([39.045753, -76.641273], 13);
+
+L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png", {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
