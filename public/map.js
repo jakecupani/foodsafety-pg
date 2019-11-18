27911e012e815df9404a2065ac0e495355647686
Jake Cupani <jakecup1@gmail.com>
2019-11-18 14:45:11 -0500

    changed basemap

--- public/map.js
+++ public/map.js
@@ -1,5 +1,5 @@
 let mymap = L.map('mapid').setView([39.045753, -76.641273], 13);
 
-L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png", {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+	attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
 }).addTo(mymap);
