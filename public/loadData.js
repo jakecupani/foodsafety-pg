67b3ab418cbed67f4cd9451695590bae318b66e0
Jake Cupani <jakecup1@gmail.com>
2019-11-18 14:28:54 -0500

    optimized marker adding

--- public/loadData.js
+++ public/loadData.js
@@ -8,12 +8,10 @@ function loadData() {
             return res;
         })
         .then(res => {
-            coordinates = new Array();
             for (i = 0; i < res.data.length; i++) {
                 let obj = res.data[i];
-                let count = 0;
                 try {
-                    coordinates.push(obj.coordinates.reverse());
+                    L.geoJSON(obj).addTo(mymap);
                     console.log(obj.coordinates);
                 }
                 catch {
@@ -22,10 +20,6 @@ function loadData() {
                 }
 
             }
-            for(k=0; k<coordinates.length; k++) {
-                L.marker(coordinates[k]).addTo(mymap);
-            }
-            // console.log(coordinates);
             return res;
         });
 }
