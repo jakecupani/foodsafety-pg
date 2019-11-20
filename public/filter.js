a1d98eecbe69a7823b99942f5a50b5df0326d028
Matt Ye <matthewye98@gmail.com>
2019-11-19 21:22:24 -0500

    added filters

---
+++ public/filter.js
@@ -0,0 +1,12 @@
+function filter() {
+  console.log('filtering');
+
+  let categoryFastfood1 = document.getElementsById("fastfood")[0].value;
+  let categoryFastfood2 = document.getElementsById("fullservice")[0].value;
+  let categoryFastfood3 = document.getElementsById("constore")[0].value;
+  let categoryFastfood4 = document.getElementsById("carryout")[0].value;
+  let categoryFastfood5 = document.getElementsById("grocstore")[0].value;
+  let categoryFastfood6 = document.getElementsById("college")[0].value;
+  let categoryFastfood7 = document.getElementsById("publicschool")[0].value;
+
+}
