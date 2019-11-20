7abf127fb09c89d17f14994ff33d8fe8afdc4fbe
Jake Cupani <jakecup1@gmail.com>
2019-11-19 22:01:54 -0500

    get checked filter values

---
+++ public/filterData.js
@@ -0,0 +1,11 @@
+function filter() {
+    console.log('filtering');
+    let checkboxes = document.getElementsByName("filter-option");
+    let filters = new Array();
+    for (i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            filters.push(checkboxes[i].value);
+        }
+    }
+    console.log(filters);
+}
