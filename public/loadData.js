a4e53fec99aaef6582d8a2a80e05512d6fff120c
Jake Cupani <jakecup1@gmail.com>
2019-11-30 17:50:40 -0500

    clear map functionality

--- public/loadData.js
+++ public/loadData.js
@@ -1,40 +1,49 @@
+let markers;
 function loadData(filters) {
     // console.log("fetch");
+    function foodList (newdata) {
+        let foodlist = "";
+        for (let i = 0; i < newdata.length; i += 1) {
+          let obj = newdata[i];
+          foodlist += "<li>"+obj.name+": "+obj.inspection_results+"</li>";
+        }
+        document.querySelector("#listing").innerHTML = "<ol>"+foodlist+"</ol>";
+        return newdata;
+      }
+    let raw_data;
+
     // confirm code is running on click
+    if(raw_data) {
+        console.log("there is data");
+    }
+    else {
+        console.log("there is not data");
+    }
     fetch('/api')
         .then(res => res.json())
         .then(res => {
-            console.log(res);
-            console.log(filters);
+            raw_data = res;
+            console.log("the raw data",raw_data);
             if (filters) {
                 console.log("there are filters");
-
                 newdata = res.data.filter(el => filters.includes(el.category));
             }
             else {
                 console.log("there are no filters");
-                return res.data;
+                newdata = res.data;
             }
-
-            function foodList (newdata) {
-              let foodlist = "";
-              for (let i = 0; i < newdata.length; i += 1) {
-                let obj = newdata[i];
-                foodlist += "<li>"+obj.name+": "+obj.inspection_results+"</li>";
-              }
-              document.querySelector("#listing").innerHTML = "<ul>"+foodlist+"<ul>"
-              return newdata;
-            }
-            foodList(newdata)
             console.log(newdata);
             return newdata;
+        
         })
         .then(res => {
             let places = new Array();
+            let names = new Array();
             for (i = 0; i < res.length; i++) {
                 let obj = res[i]["geocoded_column_1"];
                 try {
                     places.push(L.geoJSON(obj).bindPopup(res[i].name));
+                    names.push(res[i].name + res[i].address_line_1);
                 }
                 catch {
                     console.log("This did not work");
@@ -42,8 +51,15 @@ function loadData(filters) {
                 }
 
             }
-            console.log(places);
-            let markers = L.layerGroup(places).addTo(mymap);
+            console.log("places",places);
+            try {
+                markers.clearLayers();
+            }
+            catch {
+                console.log('no markers made yet');
+            }
+            markers = L.layerGroup(places).addTo(mymap);
+            foodList(res);
             return res;
         });
 }
