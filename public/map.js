b503fe08f2564c8afea7335deae176bdc5530a51
Jake Cupani <jakecup1@gmail.com>
2019-12-02 11:07:38 -0500

    map center updated

--- public/map.js
+++ public/map.js
@@ -1,4 +1,4 @@
-let mymap = L.map('mapid').setView([39.045753, -76.641273], 13);
+let mymap = L.map('mapid').setView([38.9282, -76.9158], 13);
 
 L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
 	attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
